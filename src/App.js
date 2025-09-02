04a29e51fee5f81e5c5e5f0c706a0897caafe153
Mug-Costanza <mugcostanza64@gmail.com>
2025-09-02 19:05:29 -0400

    done

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import { loadMNISTModel, predictDigitsFromDataURL } from "./ml/mnist";
 import Calendar from "./Calendar";
 import GraphView from "./GraphView";
 import "./App.css";
+import CookieConsent from "react-cookie-consent";
 
 // Function to generate a seeded random number
 function seededRandom(seed) {
@@ -383,6 +384,16 @@ function App() {
           <div className="app-footer">
                  <a href="/privacy-policy.html" className="privacy-policy-link">Privacy Policy</a>
              </div>
+          <CookieConsent
+                    location="bottom"
+                    buttonText="I understand"
+                    cookieName="myWebsiteCookieConsent"
+                    style={{ background: "#2B373B" }}
+                    buttonStyle={{ color: "#4e503b", fontSize: "13px" }}
+                    expires={150}
+                  >
+                    This website uses cookies to enhance the user experience.
+                  </CookieConsent>
       </div>
     </>
   );
